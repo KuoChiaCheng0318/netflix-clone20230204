b8af3499438b9e57ce9b3586fffdacde604f00dc
KuoChiaCheng0318 <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-02-06 13:12:19 -0700

    Work on banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios'
 import requests from './requests'
+import "./Banner.css"
 
 function Banner() {
     const [movie, setMovie] = useState([])
@@ -16,7 +17,11 @@ function Banner() {
         fetchData()
     }, [])
 
-    console.log(movie)
+    // console.log(movie)
+
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
 
     return (
         <header className="banner"
@@ -28,11 +33,18 @@ function Banner() {
             }}
         >
             <div className='banner__contents'>
-                <h1>
+                <h1 className='banner__title'>
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
+                <div className='banner__buttons'>
+                    <button className='banner__button'>Play</button>
+                    <button className='banner__button'>My List</button>
+                </div>
+                <h1 className='banner__description'>
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
-
+            <div className="banner--fadeBottom"></div>
         </header>
     )
 }
--- src/Row.js
+++ src/Row.js
@@ -23,11 +23,11 @@ function Row({title, fetchUrl, isLargeRow}) {
     return (
         <div className='row'>
             <h2>{title}</h2>
-            <div className={`rows__posters ${isLargeRow && "row__posterLarge"}`} >
+            <div className="row__posters">
                 {movies.map(movie => (
                     <img
                         key={movie.id}
-                        className='row__poster'
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
                         src={`${base_url}${isLargeRow? movie.poster_path : movie.backdrop_path}`}
                         alt={movie.name} />
                 ))}
