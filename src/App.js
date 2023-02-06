22cbb05a3c1504e7c350a9dc88991c4241ee7434
KuoChiaCheng0318 <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-02-05 21:22:42 -0700

    fetch movie data from tmdb api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix clone app</h1>
+      <Row title="ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
